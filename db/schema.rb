5de6cdce9d15afdb085f0c4673d52665d1e716d0
justinhornung <jhornung328i@gmail.com>
2023-02-09 21:59:56 -0500

    Update Schema to allow user to change completion status

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_012451) do
     t.string "title"
     t.string "description"
     t.string "deadline"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
